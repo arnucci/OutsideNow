f13c0799cf3496ae91218f8f68ed5e56ae4b3e58
arnucci <arnaud.salvucci@gmail.com>
2014-04-13 12:19:35 +0200

    filter is more restrictive

--- index.php
+++ index.php
@@ -13,14 +13,14 @@ $client->getClient()->setUserAgent('Mozilla/5.0 (X11; U; Linux i686; fr; rv:1.9b
 $crawler = $client->request('GET', 'http://www.espace-julien.com/fr/affiche');
 
 //le programme est contenu dans des tables. Un table par mois.
-$results = $crawler->filter('table')->each(function (Crawler $node, $i) {
+$results = $crawler->filter('table>tr')->each(function (Crawler $node, $i) {
     return $node->html();
 });
 
 
 foreach ($results as $domElement) {
 
-    var_dump($domElement)."\n";
+    print_r($domElement);
 }
 
 
