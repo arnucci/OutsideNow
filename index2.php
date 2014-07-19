f518b2bd1485e72d5cd9921809aeb7ea15fb8ff2
arnucci <arnaud.salvucci@gmail.com>
2014-07-19 18:48:53 +0200

    manage exception in case of + in the title (not satisfied)

--- index2.php
+++ index2.php
@@ -16,19 +16,28 @@ $nodes = $crawler->filter('div#MEP-faux2PG-coldroite')->each(function ($node) {
     return $node;
     });
 
-$eventArray = array();
-$i = 0;
+ $eventArray = array();
+ $i = 0;
 
-foreach ($nodes as $node) {
+ foreach ($nodes as $node) {
 
-    $eventArray[$i]['date']  = $node->filter('.TXT-Date')->text();
-    $eventArray[$i]['group'] = $node->filter('.TXT-Groupe')->text();
-    $eventArray[$i]['hour']  = substr($node->filter('.TXT-Horaire-Tarif')->text(), 1, strpos($node->filter('.TXT-Horaire-Tarif')->text(), '~') -2);
-    $eventArray[$i]['price'] = substr($node->filter('.TXT-Horaire-Tarif')->text(), strpos($node->filter('.TXT-Horaire-Tarif')->text(), '~') + 2);
-    $eventArray[$i]['link']  = 'http://www.leposteagalene.com/pages/programme.html';
+     $eventArray[$i]['date']  = $node->filter('.TXT-Date')->text();
 
-    $i++;
-}
+     try {     
+         $eventArray[$i]['group'] = $node->filter('.TXT-Groupe')->text();
 
-var_dump($eventArray);
+     }         catch (Exception $e) {
+
+             $eventArray[$i]['group'] ='coucou';
+         }
+
+     
+     $eventArray[$i]['hour']  = substr($node->filter('.TXT-Horaire-Tarif')->text(), 1, strpos($node->filter('.TXT-Horaire-Tarif')->text(), '~') -2);
+     $eventArray[$i]['price'] = substr($node->filter('.TXT-Horaire-Tarif')->text(), strpos($node->filter('.TXT-Horaire-Tarif')->text(), '~') + 2);
+     $eventArray[$i]['link']  = 'http://www.leposteagalene.com/pages/programme.html';
+
+     $i++;
+ }
+
+ var_dump($eventArray);
 
