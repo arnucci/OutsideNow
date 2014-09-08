d2ae4656c9d115a2713660f335a5ce632d139d1d
arnucci <arnaud.salvucci@gmail.com>
2014-09-08 22:54:35 +0200

    improve the inclusion of spiders

--- index.php
+++ index.php
@@ -1,48 +1,18 @@
 <?php
-
 require_once 'vendor/autoload.php';
 
 use Goutte\Client;
 use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\Finder\Finder;
 
 $client = new Client();
 
 $client->getClient()->setUserAgent('Mozilla/5.0 (X11; U; Linux i686; fr; rv:1.9b5) Gecko/2008041514 Firefox/3.0b5');
 
-switch ($argv[1]) {
-
- case 'marseille':
-     require_once 'spiders/marseille-espace_julien.php';
-     require_once 'spiders/marseille-poste_a_galene.php';
-     require_once 'spiders/marseille-silo.php';
-     break;
-
- case 'espace_julien':
-     require_once 'spiders/marseille-espace_julien.php';
-     break;
-
- case 'poste_a_galene':
-     require_once 'spiders/marseille-poste_a_galene.php';
-     break;
-
- case 'silo':
-     require_once 'spiders/marseille-silo.php';
-     break;
- }
-
-
-use Symfony\Component\Finder\Finder;
-
 $finder = new Finder();
-$finder->files()->in(__DIR__);
+$finder->files()->name('*'.$argv[1].'*')->in('spiders/');
 
 foreach ($finder as $file) {
-    // Print the absolute path
-    print $file->getRealpath()."\n";
 
-    // Print the relative path to the file, omitting the filename
-    print $file->getRelativePath()."\n";
-
-    // Print the relative path to the file
-    print $file->getRelativePathname()."\n";
+    require 'spiders/'.$file->getRelativePathname();
 }
