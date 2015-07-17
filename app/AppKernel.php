db41a1f3ae8842b1ce0d307d062b9c614b20ff53
arnucci <arnaud.salvucci@gmail.Com>
2015-07-17 22:14:01 +0200

    create Event bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new AppBundle\AppBundle(),
+            new OutsideNow\EventBundle\OutsideNowEventBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/OutsideNow/EventBundle/OutsideNowEventBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace OutsideNow\EventBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OutsideNowEventBundle extends Bundle
+{
+}
